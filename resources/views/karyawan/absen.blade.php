ea74746b6b4e00b9ba31bb06114f6727de96fdee
Ramdona Kamaliah <ramdonakamaliah@gmail.com>
2025-01-07 07:27:05 +0700

    ini

--- resources/views/karyawan/absen.blade.php
+++ resources/views/karyawan/absen.blade.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Absen</title>
 </head>
+
 <body>
     <h1>Halaman Absen</h1>
+    <h2>hahaha</h2>
 </body>
+
 </html>
