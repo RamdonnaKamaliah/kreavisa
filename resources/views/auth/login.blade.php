6bd0848832453a2bf20d3e9a20b6bf9e07ff0df3
Ramdona Kamaliah <ramdonakamaliah@gmail.com>
2025-01-12 14:41:00 +0700

    ini

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,4 @@
 <x-guest-layout>
-    <div class="text-center mb-4">
-        <h1 class="h3 text-gray-900"><i class="fa-solid fa-r"></i> Login</h1>
-    </div>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -10,13 +7,18 @@
 
         <!-- Email Address -->
         <div class="form-group">
-            <input type="email" class="form-control form-control-user" id="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="Enter Email Address...">
+            <input type="email"
+                class=" text-sm w-full px-4 py-2 border border-gray-700 rounded-full  focus:outline-none focus:ring-2 focus:ring-[#FD7170] focus:border-transparent placeholder-gray-400 "
+                id="email" name="email" :value="old('email')" required autofocus autocomplete="username"
+                placeholder="Enter Email Address...">
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="form-group">
-            <input type="password" class="form-control form-control-user" id="password" name="password" required autocomplete="current-password" placeholder="Enter Password...">
+            <input type="password"
+                class="text-sm  w-full px-4 py-2 border border-gray-700 rounded-full focus:outline-none focus:ring-2 focus:ring-[#FD7170] focus:border-transparent placeholder-gray-400"
+                id="password" name="password" required autocomplete="current-password" placeholder="Enter Password...">
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
@@ -29,19 +31,14 @@
         </div>
 
         <!-- Login Button -->
-        <button type="submit" class="btn btn-primary btn-user btn-block mb-4">
+        <button type="submit"
+            class="mb-4 bg-[#FD7170] text-white font-semibold py-2 px-56 rounded-full shadow-md hover:shadow-lg hover:bg-opacity-90 transition duration-300 ease-in-out transform hover:scale-105">
             {{ __('Log in') }}
         </button>
 
         <hr>
 
-        <!-- Additional Login Options -->
-        <div class="d-flex justify-content-center mt-4">
-            <a href="mailto:rifdah.a122@gmail.com" class="btn btn-outline-primary btn-circle mx-2">
-                <i class="fas fa-envelope"></i>
-            </a>
-            
-        </div>
+
 
         <!-- Forgot Password -->
         <div class="text-center mt-3">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,40 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="sweetalert2.min.css">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Custom fonts for this template-->
     <!-- Di dalam <head> -->
-		<link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link
-    
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{ asset('asset-landing-admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
 
-	 <!-- Scripts -->
-	 @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="d-flex align-items-center min-vh-100" style="background: linear-gradient(to right,  #2d2ee9, #b7229a);">
+<body class="d-flex align-items-center min-vh-100" style="background: linear-gradient(to right,  #FD7170, #FDC5B5);">
     <div class="container">
         <!-- Outer Row -->
         <div class="row justify-content-center">
@@ -42,7 +42,7 @@
                 <div class="card o-hidden border-0 shadow-lg">
                     <div class="card-body p-5">
                         <!-- Form Content -->
-                       
+
                         {{ $slot }}
                     </div>
                 </div>
@@ -51,7 +51,7 @@
     </div>
 
     <!-- Scripts -->
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('asset-landing-admin/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('asset-landing-admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
